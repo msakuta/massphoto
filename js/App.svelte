55effa6553a792d47da760d4e493b6e06735d751
msakuta <masahiro.sakuta@gmail.com>
2022-06-29 01:59:33 +0900

    Add header buttons (not functional yet)

--- js/App.svelte
+++ js/App.svelte
@@ -18,28 +18,53 @@
 	window.addEventListener('load', () => getFileList(rootPath));
 </script>
 
-<p>These styles...</p>
+<div class="header">
+	<div class="path" id="path">{rootPath}</div>
+	<div class="iconContainer">
+		<img class="icon" alt="home" id="homeButton" src={`${baseUrl}/home.png`}>
+		<img class="icon" alt="up (U)" id="upButton" src={`${baseUrl}/up.png`}>
+		<img class="icon" alt="previous (H)" id="leftButton" src={`${baseUrl}/left.png`}>
+		<img class="icon" alt="next (K)" id="rightButton" src={`${baseUrl}/right.png`}>
+	</div>
+</div>
 
-<ul>
-</ul>
-
-<div class='dirContainer' id="thumbnails">
-	{#each dirList as dir}
-		<Thumbnail {dir} {rootPath} {baseUrl}/>
-	{/each}
-	{#each fileList as file}
-		<Thumbnail image={file} {rootPath} {baseUrl}/>
-	{/each}
+<div class="scrollContents">
+	<div class='dirContainer' id="thumbnails">
+		{#each dirList as dir}
+			<Thumbnail {dir} {rootPath} {baseUrl}/>
+		{/each}
+		{#each fileList as file}
+			<Thumbnail image={file} {rootPath} {baseUrl}/>
+		{/each}
+	</div>
 </div>
 
 
-
 <style>
-	p {
-		color: purple;
-		font-family: 'Times New Roman', cursive;
-		font-size: 2em;
+	.header {
+		position: fixed;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: 3em;
+		background-color: rgba(191, 191, 191, 0.75);
+		z-index: 100;
 	}
+
+	.path {
+		font-size: 1.5em;
+	}
+
+	.iconContainer {
+		position: absolute;
+		top: 0;
+		right: 0;
+	}
+
+	.scrollContents {
+		margin-top: 3em;
+	}
+
 	.dirContainer {
 		display: flex;
 		flex-wrap: wrap;
