5de65342c3c487aaf094a456f85ca6540c14f2cf
msakuta <masahiro.sakuta@gmail.com>
2022-06-30 21:20:39 +0900

    Allow mouse wheel and dragging to move zoomed image

--- js/ImageView.svelte
+++ js/ImageView.svelte
@@ -4,23 +4,70 @@
     const dispatch = createEventDispatcher();
 
     export let imagePath = "";
+    let scale = 1.;
+    let imageTransform = "";
+    let translate = [300, 300];
+
+    function applyZoom(event){
+        if(focus === null) return true;
+        event.preventDefault();
+
+        var deltaScale = Math.exp(event.deltaY < 0 ? 0.1 : -0.1);
+
+        var x = event.clientX;
+        var y = event.clientY;
+        // translate[0] = -x * deltaScale + x;
+        // translate[1] = -y * deltaScale + y;
+
+        scale *= deltaScale;
+
+        // Restrict scale
+        scale = Math.min(Math.max(0.1, scale), 20);
+
+        // Apply scale transform
+        imageTransform = `translate(${translate[0]}px, ${translate[1]}px) scale(${scale})`;
+    }
+
+    let dragStart = null;
+    let dragMoved = false;
+    function mousedown(event) {
+        event.preventDefault();
+        dragStart = [event.clientX, event.clientY];
+    }
 
     function mouseup(event) {
-        event.preventDefault();
-        if (event.button === 0) {
+        if(!dragMoved && event.button === 0){
             dispatch('defocus');
+            event.preventDefault();
         }
-        return false;
+        dragStart = null;
+        dragMoved = false;
     }
 
     function contextmenu(event) {
         event.preventDefault();
     }
+
+    function mousemove(event) {
+        if(dragStart && focus){
+            translate[0] += event.clientX - dragStart[0];
+            translate[1] += event.clientY - dragStart[1];
+            imageTransform = `translate(${translate[0]}px, ${translate[1]}px) scale(${scale})`;
+            dragStart = [event.clientX, event.clientY];
+            dragMoved = true;
+        }
+    }
+
+    function mouseleave() {
+        dragStart = null;
+        dragMoved = false;
+    }
+
 </script>
 
-<div>
-    <img class="zoomInt" src={imagePath}
-        on:mouseup={mouseup} on:contextmenu={contextmenu} alt={imagePath}>
+<div style="transform: {imageTransform}" on:wheel={applyZoom}
+        on:mouseup={mouseup} on:contextmenu={contextmenu} on:mousedown={mousedown} on:mousemove={mousemove} on:mouseleave={mouseleave}>
+    <img class="zoomInt noPointer" src={imagePath} alt={imagePath}>
 </div>
 
 <style>
@@ -28,5 +75,8 @@
         width: 100%;
         height: 100%;
         object-fit: contain;
+    }
+    .noPointer {
+        pointer-events: none;
     }
 </style>
